5eb05ca29cad7cdfb4aaff2b7fe8830c7156f20d
numme <nummer14@naver.com>
2025-06-04 11:29:08 +0900

    First commit

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,9 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int a = 10;
-        int b = 7;
+        int number = 5;
 
-        System.out.println("a == b : " + (a == b));
-        System.out.println("a != b : " + (a != b));
-
-        System.out.println("a > b : " + (a > b));
-        System.out.println("a < b : " + (a <b));
-        System.out.println("a >= 10 : " + (a >= 10));
-        System.out.println("b <= 5 : " + (b <= 5));
+        for (int i = 1; i <= 9; i++ ) {
+            System.out.println(number + " x " + i +" = " + (number * i));
+        }
     }
 }
