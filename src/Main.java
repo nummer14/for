a9563aa6f98af65458519f5db6efa529755cd279
numme <nummer14@naver.com>
2025-06-04 11:06:23 +0900

    First commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 7;
+
+        System.out.println("a == b : " + (a == b));
+        System.out.println("a != b : " + (a != b));
+
+        System.out.println("a > b : " + (a > b));
+        System.out.println("a < b : " + (a <b));
+        System.out.println("a >= 10 : " + (a >= 10));
+        System.out.println("b <= 5 : " + (b <= 5));
+    }
+}
